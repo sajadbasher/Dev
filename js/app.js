f74137a978e5b6912ec048177d8d8bdaa18e9501
sajadbasher <104499822+sajadbasher@users.noreply.github.com>
2023-11-13 10:05:42 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,73 +1,4 @@
-const container = document.querySelector(".container");
 
-
-
-function getUserMedia(options, successCallback, failureCallback) {
-  var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia || navigator.msGetUserMedia;
-  if (api) {
-    return api.bind(navigator)(options, successCallback, failureCallback);
-  }
-}
-
-var theStream;
-
-function getStream() {
-  if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-    alert('User Media API not supported.');
-    return;
-  }
-  
-  var constraints = {
-    video: true
-  };
-
-  getUserMedia(constraints, function (stream) {
-    var mediaControl = document.querySelector('video');
-    if ('srcObject' in mediaControl) {
-      mediaControl.srcObject = stream;
-    } else if (navigator.mozGetUserMedia) {
-      mediaControl.mozSrcObject = stream;
-    } else {
-      mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-    }
-    theStream = stream;
-  }, function (err) {
-    alert('Error: ' + err);
-  });
-}
-
-function takePhoto() {
-  if (!('ImageCapture' in window)) {
-    alert('ImageCapture is not available');
-    return;
-  }
-  
-  if (!theStream) {
-    alert('Grab the video stream first!');
-    return;
-  }
-  
-  var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
-
-  theImageCapturer.takePhoto()
-    .then(blob => {
-      var theImageTag = document.getElementById("imageTag");
-      theImageTag.src = URL.createObjectURL(blob);
-      window.sessionStorage
-      localStorage.setItem("MyPicture", URL.createObjectURL(blob))// Save the image to the Cache API
-    })
-    .catch(err => alert('Error: ' + err));
-}
-
-function getPic(){
-  var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
-  window.sessionStorage
-  var theImageTag = document.getElementById("imageTag2");
-  theImageTag.src = localStorage.getItem("MyPicture");
-
-}
 
 
 if ("serviceWorker" in navigator) {
@@ -78,3 +9,6 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err));
   });
 }
+
+
+// Geolocation
