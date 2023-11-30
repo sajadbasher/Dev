0392bb011f1377fbcacf327cea3997b06cf717df
sajadbasher <104499822+sajadbasher@users.noreply.github.com>
2023-11-30 16:47:47 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,7 +22,30 @@ document.addEventListener('DOMContentLoaded', function() {
         updateExpenseList();
     }
 
-    // Andere Codeabschnitte...
+    // Initialisierung der Ausgaben
+    let expenses = [];
+
+    function updateExpenseList() {
+        const tableBody = document.getElementById('expenseTableBody');
+        const balanceSpan = document.getElementById('balance');
+
+        // Clear previous entries
+        tableBody.innerHTML = '';
+
+        // Populate the table
+        let totalAmount = 0;
+        expenses.forEach(expense => {
+            const row = tableBody.insertRow();
+            row.insertCell(0).innerText = expense.description;
+            row.insertCell(1).innerText = expense.date;
+            row.insertCell(2).innerText = expense.category;
+            row.insertCell(3).innerText = expense.amount.toFixed(2);
+            totalAmount += expense.amount;
+        });
+
+        // Update balance
+        balanceSpan.innerText = totalAmount.toFixed(2);
+    }
 
     // Event-Listener für den Button hinzufügen
     document.getElementById('addExpenseButton').addEventListener('click', addExpense);
