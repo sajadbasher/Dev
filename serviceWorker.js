a52bb2af69d653ed317ab2c945bc8e109906f7e5
sajadbasher <104499822+sajadbasher@users.noreply.github.com>
2024-01-24 09:01:24 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+/*const staticDevCoffee = "dev-coffee-site-v1";
 const assets = [
   "/",
   "/index.html",
@@ -30,3 +30,254 @@ self.addEventListener("fetch", fetchEvent => {
     })
   );
 });
+*/
+
+
+function getUserMedia(constraints) {
+  // if Promise-based API is available, use it
+  if (navigator.mediaDevices) {
+    return navigator.mediaDevices.getUserMedia(constraints);
+  }
+    
+  // otherwise try falling back to old, possibly prefixed API...
+  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    
+  if (legacyApi) {
+    // ...and promisify it
+    return new Promise(function (resolve, reject) {
+      legacyApi.bind(navigator)(constraints, resolve, reject);
+    });
+  }
+}
+
+function getStream (type) {
+  if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+    alert('User Media API not supported.');
+    return;
+  }
+
+  var constraints = {};
+  constraints[type] = true;
+
+  function getUserMedia(options, successCallback, failureCallback) {
+  var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+  if (api) {
+    return api.bind(navigator)(options, successCallback, failureCallback);
+  }
+}
+
+var theStream;
+
+function getStream() {
+  if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+    alert('User Media API not supported.');
+    return;
+  }
+  
+  var constraints = {
+    video: true
+  };
+
+  getUserMedia(constraints, function (stream) {
+    var mediaControl = document.querySelector('video');
+    if ('srcObject' in mediaControl) {
+      mediaControl.srcObject = stream;
+    } else if (navigator.mozGetUserMedia) {
+      mediaControl.mozSrcObject = stream;
+    } else {
+      mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+    }
+    theStream = stream;
+  }, function (err) {
+    alert('Error: ' + err);
+  });
+}
+
+function takePhoto() {
+  if (!('ImageCapture' in window)) {
+    alert('ImageCapture is not available');
+    return;
+  }
+  
+  if (!theStream) {
+    alert('Grab the video stream first!');
+    return;
+  }
+  
+  var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
+
+  theImageCapturer.takePhoto()
+    .then(blob => {
+      var theImageTag = document.getElementById("imageTag");
+      theImageTag.src = URL.createObjectURL(blob);
+    })
+    .catch(err => alert('Error: ' + err));
+
+  function getUserMedia(options, successCallback, failureCallback) {
+  var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+  if (api) {
+    return api.bind(navigator)(options, successCallback, failureCallback);
+  }
+}
+var theStream;
+var theRecorder;
+var recordedChunks = [];
+
+function getStream() {
+  if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+    alert('User Media API not supported.');
+    return;
+  }
+  
+  var constraints = {video: true, audio: true};
+  getUserMedia(constraints, function (stream) {
+    var mediaControl = document.querySelector('video');
+    
+    if ('srcObject' in mediaControl) {
+      mediaControl.srcObject = stream;
+    } else if (navigator.mozGetUserMedia) {
+      mediaControl.mozSrcObject = stream;
+    } else {
+      mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+    }
+    
+    theStream = stream;
+    try {
+      recorder = new MediaRecorder(stream, {mimeType : "video/webm"});
+    } catch (e) {
+      console.error('Exception while creating MediaRecorder: ' + e);
+      return;
+    }
+    theRecorder = recorder;
+    console.log('MediaRecorder created');
+    recorder.ondataavailable = recorderOnDataAvailable;
+    recorder.start(100);
+  }, function (err) {
+    alert('Error: ' + err);
+  });
+}
+
+function recorderOnDataAvailable(event) {
+  if (event.data.size == 0) return;
+  recordedChunks.push(event.data);
+}
+
+function download() {
+  console.log('Saving data');
+  theRecorder.stop();
+  theStream.getTracks()[0].stop();
+
+  var blob = new Blob(recordedChunks, {type: "video/webm"});
+  var url = (window.URL || window.webkitURL).createObjectURL(blob);
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = 'test.webm';
+  a.click();
+  
+  // setTimeout() here is needed for Firefox.
+  setTimeout(function () {
+      (window.URL || window.webkitURL).revokeObjectURL(url);
+  }, 100); 
+
+  function getUserMedia(options, successCallback, failureCallback) {
+  var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+  if (api) {
+    return api.bind(navigator)(options, successCallback, failureCallback);
+  }
+}
+
+var pc1;
+var pc2;
+var theStreamB;
+
+function getStream() {
+  if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+    alert('User Media API not supported.');
+    return;
+  }
+  
+  var constraints = {
+    video: true
+  };
+  getUserMedia(constraints, function (stream) {
+    addStreamToVideoTag(stream, 'localVideo');
+
+    // RTCPeerConnection is prefixed in Blink-based browsers.
+    window.RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection;
+    pc1 = new RTCPeerConnection(null);
+    pc1.addStream(stream);
+    pc1.onicecandidate = event => {
+      if (event.candidate == null) return;
+      pc2.addIceCandidate(new RTCIceCandidate(event.candidate));
+    };
+
+    pc2 = new RTCPeerConnection(null);
+    pc2.onaddstream = event => {
+      theStreamB = event.stream;
+      addStreamToVideoTag(event.stream, 'remoteVideo');
+    };
+    pc2.onicecandidate = event => {
+      if (event.candidate == null) return;
+      pc1.addIceCandidate(new RTCIceCandidate(event.candidate));
+    };
+
+    pc1.createOffer({offerToReceiveVideo: 1})
+      .then(desc => {
+        pc1.setLocalDescription(desc);
+        pc2.setRemoteDescription(desc);
+        return pc2.createAnswer({offerToReceiveVideo: 1});
+      })
+      .then(desc => {
+        pc1.setRemoteDescription(desc);
+        pc2.setLocalDescription(desc);
+      })
+      .catch(err => {
+        console.error('createOffer()/createAnswer() failed ' + err);
+      });
+  }, function (err) {
+    alert('Error: ' + err);
+  });
+}
+
+function addStreamToVideoTag(stream, tag) {
+  var mediaControl = document.getElementById(tag);
+  if ('srcObject' in mediaControl) {
+    mediaControl.srcObject = stream;
+  } else if (navigator.mozGetUserMedia) {
+    mediaControl.mozSrcObject = stream;
+  } else {
+    mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+  }
+
+  if ('serviceWorker' in navigator) {
+    navigator.ServiceWorker
+              .register('./service-worker.js')
+              .then(function() {console.log('Service Worker Registered'); });
+  }
+  var cacheName = 'weatherPWA-step-6-1';
+  var filesToCache = [];
+
+  self.addEventListener ('install', function(e) {
+    console.log (' [ServiceWorker] Install');
+    e.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+         console.log('[ServiceWorker] Installed');
+        return cache.addAll (filesToCache);
+      })
+      );
+  });
+self. addEventListener ('activate'. function(e) {
+console.log('[ServiceWorker] Activate');
+});
+
